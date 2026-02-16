e711fbbb7e7a355010c9a6a69a7e6cbdba3e6220
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-16 17:59:17 +0700

    Update 01_SmartFitting.ino

--- examples/01_SmartFitting/01_SmartFitting.ino
+++ examples/01_SmartFitting/01_SmartFitting.ino
@@ -1,7 +1,8 @@
 #include <IskakINO_ArduFast.h>
 #include <IskakINO_WifiPortal.h>
 #include <IskakINO_FastNTP.h>
-#include <IskakINO_Storage.h> // Library baru Anda
+#include <IskakINO_Storage.h> 
+#include <WiFiUdp.h>
 
 // Struktur data untuk disimpan
 struct SystemConfig {
@@ -11,16 +12,39 @@ struct SystemConfig {
 
 SystemConfig config;
 
+// Inisialisasi Objects berdasarkan header library Anda
+IskakINO_WifiPortal portal;
+WiFiUDP ntpUDP;
+IskakINO_FastNTP ntp(ntpUDP, "pool.ntp.org"); // Membutuhkan UDP
+
 void setup() {
+  // ArduFast menggunakan instance global 'ArduFast'
+  ArduFast.begin(115200); 
+  
+  // IskakStorage menggunakan instance global 'IskakStorage'
   IskakStorage.begin("smartfit", true);
   
   // Load data dari alamat 0
   if (!IskakStorage.load(0, config)) {
-    // Jika gagal (data baru/kosong), set default
     config.lampState = false;
     config.bootCount = 0;
   }
   
   config.bootCount++;
   IskakStorage.save(0, config);
+
+  // Inisialisasi Portal & NTP
+  portal.begin("IskakINO-Project01");
+  ntp.begin();
+}
+
+void loop() {
+  // WAJIB ADA agar tidak error 'undefined reference to loop'
+  portal.handle(); // Menangani WebServer & DNS
+  ntp.update();   // Update waktu secara non-blocking
+  
+  // Contoh penggunaan ArduFast scheduler
+  if (ArduFast.every(5000, 0)) {
+     ArduFast.log(F("System Uptime (s)"), ntp.getUptimeSeconds());
+  }
 }
