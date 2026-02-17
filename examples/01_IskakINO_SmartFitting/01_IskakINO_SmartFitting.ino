f2d656f336100af3a781b6c30a057241b724e354
Iskak Fatoni <iskakfatoni@gmail.com>
2026-02-17 15:54:01 +0700

    Update 01_IskakINO_SmartFitting.ino

--- examples/01_IskakINO_SmartFitting/01_IskakINO_SmartFitting.ino
+++ examples/01_IskakINO_SmartFitting/01_IskakINO_SmartFitting.ino
@@ -1,4 +1,5 @@
 /************************************************************
+
  * PROJECT    : Smart Fitting Lamp IskakINO
  * BOARD      : ESP32-C3 / ESP8266
  * VERSION    : v1.5.5 (Stable Build)
